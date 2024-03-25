5318f04de5fa0c4b72044f74c12502bde94cca46
Captain <mackenzie.me@icloud.com>
2024-03-25 18:50:20 +0700

    quiz test

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netFoodPrice = "15000";
+const restaurantCurrentlyOpen = "1500"
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd";
+netFoodPrice = "5000";
+restaurantCurrentlyOpen = "2500";
